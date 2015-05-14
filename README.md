09692a8ffe339716ac3ec41ef621194fcfd88e07
Brendon Rapp <brendon@silentgrowl.com>
2015-05-14 11:05:29 -0700

    Extracted from Isis

---
+++ README.md
@@ -0,0 +1,19 @@
+# Isis plugin: Bash.org
+
+This plugin adds a command for random [bash.org](http://bash.org) quotes to the [Isis chatbot](https://github.com/silentgrowl/isis)
+
+## Installation
+
+Add this line to your Isis installation's Gemfile:
+
+``ruby
+gem 'isis-plugin-bash'
+``
+
+And then execute:
+
+    $ bundle
+
+## Usage
+
+Trigger a random bash.org quite by typing ``!bash`` in chat.`
---
+++ lib/isis/plugin/bash.rb
@@ -0,0 +1,22 @@
+require "nokogiri"
+
+module Isis
+  module Plugin
+    class Bash < Isis::Plugin::Base
+      def respond_to_msg?(msg, speaker)
+        /\b!bash\b/i =~ msg
+      end
+
+      private
+
+      def response_text
+        page = Nokogiri.HTML(open('http://bash.org?random1'))
+        selected = rand(page.css('.quote').length)
+        link = page.css('.quote')[selected].css('a').first['href']
+        quote = page.css('.qt')[selected].text
+        number = link.gsub('?', '')
+        "bash.org ##{number}: \r\n#{quote}\r\n(link: http://bash.org#{link})"
+      end
+    end
+  end
+end
